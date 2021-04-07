4555d463040cce599388738fada413f76d5bed84
John <jooke98@outlook.com>
2021-04-06 21:39:55 -0600

    static server working

--- http-server/main.go
+++ http-server/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	const port = 3000
+	const port = "3000"
 	fmt.Println("Listening on port", port)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to the server!")
+	})
+
+	http.ListenAndServe(":"+port, nil)
 }
